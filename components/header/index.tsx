1e860dacf34a4a6ccc36a0836a5fbe1daab90f88
iamfiro <appturbo102@gmail.com>
2024-04-10 23:23:36 +0900

    💄 Refactor header component and styles, and remove unused code

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,32 +1,7 @@
-import { borderRadiusValue, HeaderButtonProps, HeaderProps } from '@/types/components';
+import { HeaderProps } from '@/types/components';
 import style from './style.module.scss';
 import Image from 'next/image';
 import { headers } from 'next/headers';
-
-export const HeaderButton = ({
-    children,
-    href,
-    backgroundColor = '#000',
-    color = '#fff',
-    borderRadius = 'small',
-    height = 40
-}: HeaderButtonProps) => {
-    const radius = borderRadiusValue[borderRadius] || '4px';
-
-    return (
-        <a 
-            href={href}
-            className={style.primaryButton}
-            style={{ 
-                backgroundColor,
-                color, 
-                borderRadius: radius,
-                height: `${height}px`
-            }}
-        >{children}</a>
-    )
-};
-
 
 const Header = ({ logo, menu, children, logoWidth = 20 }: HeaderProps) => {
     const path = headers().get('x-pathname') || '/';
