fa271611ea9674867587e9135685109eb0912383
iamfiro <appturbo102@gmail.com>
2024-04-10 23:23:20 +0900

    💄 Update spacing component to include width and dev mode

--- components/spacing/index.tsx
+++ components/spacing/index.tsx
@@ -1,6 +1,8 @@
-export const Spacing = ({ height = 10 }: { height: number }) => {
+export const Spacing = ({ height=10, width = 10 }: { height?: number, width?: number }) => {
+    const devMode = process.env.SPACING_DEV_MODE === 'true';
+
     return (
-        <div style={{ height }}/>
+        <div style={{ height, width, backgroundColor: devMode ? 'var(--color-red-lighter)' : '', margin: '0 auto' }}>{devMode}</div>
     )
 }
 
