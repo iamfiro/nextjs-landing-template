d18b3ec0627a21719bdb24451fe119f65c015ade
iamfiro <appturbo102@gmail.com>
2024-04-10 23:23:08 +0900

    ✨ Add Button component and styles

---
+++ components/button/index.tsx
@@ -0,0 +1,32 @@
+import { borderRadiusValue, ButtonProps } from "@/types/components";
+import style from './style.module.scss';
+
+const Button = ({
+    children,
+    href,
+    backgroundColor = '#000',
+    color = '#fff',
+    borderRadius = 'small',
+    height = 40,
+    fontSize = 14,
+    styles
+}: ButtonProps) => {
+    const radius = borderRadiusValue[borderRadius] || '4px';
+
+    return (
+        <a
+            href={href}
+            className={style.button}
+            style={{ 
+                backgroundColor,
+                color, 
+                borderRadius: radius,
+                height: `${height}px`,
+                fontSize,
+                ...styles
+            }}
+        >{children}</a>
+    )
+};
+
+export default Button;
