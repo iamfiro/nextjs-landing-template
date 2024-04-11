c19ab4d797528b3d01a92fc4846455f1804d00da
iamfiro <appturbo102@gmail.com>
2024-04-11 18:27:01 +0900

    💄 Add Sponsor component and styles, and update page.tsx to include Sponsor component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
-import { Button, Header, IntroTitle, Spacing } from "@/components";
+import { Button, Header, IntroTitle, Spacing, Sponsor } from "@/components";
 import Logo from "@/public/logo.png";
+import GoogleLogo from '@/public/sample-google.webp'
 
 export default function Home() {
     const headerPath = [
@@ -8,6 +9,37 @@ export default function Home() {
         { name: '고객센터', href: '/contact' },
         { name: '회사소개', href: '/' },
     ];
+
+    const SponsorList = [
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+        {
+            name: 'Google',
+            logo: GoogleLogo
+        },
+    ]
 	return (
 		<main>
             <Header logo={Logo} backgroundColor="#000000" menu={headerPath}>
@@ -15,9 +47,11 @@ export default function Home() {
                 <Button href="/login" borderRadius="medium">무료로 시작하기</Button>
             </Header>
             <Spacing height={100} />
-            <IntroTitle type="column" title="고객 상담의 미래는 AI입니다" description="세계 최대 기업에서 사용하는 Next.js를 사용하면 React 구성 요소의 강력한 기능으로 고품질 웹 애플리케이션을 만들 수 있습니다." titleFontSize={55}>
+            <IntroTitle type="column" title="무료 Next.js 랜딩 페이지 템플릿" description="세계 최대 기업에서 사용하는 Next.js를 사용하면 React 구성 요소의 강력한 기능으로 고품질 웹 애플리케이션을 만들 수 있습니다." titleFontSize={55}>
                 <Button href="/login" fontSize={18} borderRadius="big" styles={{ padding: '5px 30px'}}>시작하기</Button>
             </IntroTitle>
+            <Spacing height={120} />
+            <Sponsor title="Trust By" SponsorList={SponsorList} grayScaleMode={true} />
 		</main>
 	);
 }
--- components/index.ts
+++ components/index.ts
@@ -2,3 +2,4 @@ export { default as Header } from "./header";
 export { default as IntroTitle } from "./introTitle";   
 export { default as Spacing } from "./spacing";
 export { default as Button } from "./button";
+export { default as Sponsor } from "./sponsor";
