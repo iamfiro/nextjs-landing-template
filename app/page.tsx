306bcf06651487ca404cbdcef31648416efa5479
FIRO <72495729+iamfiro@users.noreply.github.com>
2024-04-11 23:53:33 +0000

    🚀 Update app.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,10 +6,10 @@ import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
     const headerPath = [
-        { name: '제품소개', href: '/product' },
-        { name: '가격안내', href: '/pricing' },
-        { name: '고객센터', href: '/contact' },
-        { name: '회사소개', href: '/' },
+        { name: 'Product', href: '/product' },
+        { name: 'Pricing', href: '/pricing' },
+        { name: 'Contact', href: '/contact' },
+        { name: 'Nothing', href: '/' },
     ];
 
     const SponsorList = [
@@ -60,7 +60,7 @@ export default function Home() {
                 <Button href="/login" borderRadius="medium"><FaGithub /> Github</Button>
             </Header>
             <Spacing height={100} />
-            <IntroTitle type="column" title="The landing page template for NEXT.js developers" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." titleFontSize={55}>
+            <IntroTitle type="row" title="The landing page template for NEXT.js developers" description="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s." titleFontSize={55}>
                 <Button href="/login" fontSize={16} borderRadius="big" styles={{ padding: '5px 30px'}}><FaGithub /> Github</Button>
             </IntroTitle>
             <Spacing height={200} />
