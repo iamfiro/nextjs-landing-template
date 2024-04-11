98ae2137ef9a24104a1d71c696e4f175d40c3116
iamfiro <appturbo102@gmail.com>
2024-04-11 18:29:24 +0900

    💄 Refactor IntroTitle component to include custom styles

--- components/introTitle/index.tsx
+++ components/introTitle/index.tsx
@@ -1,12 +1,12 @@
 import { IntroTitleProps } from '@/types/components';
 import style from './style.module.scss'
 
-export const IntroTitle = ({ type, title, description, titleFontSize = 40, children }: IntroTitleProps) => {
+export const IntroTitle = ({ type, title, description, titleFontSize = 40, children, styles }: IntroTitleProps) => {
     const fontSize = `${titleFontSize}px`;
     switch (type) {
         case 'column':
             return (
-                <section className={style.column}>
+                <section className={style.column} style={styles}>
                     <h1 className={style.column_title} style={{ fontSize: fontSize, lineHeight: fontSize }}>{title}</h1>
                     <span className={style.column_description}>{description}</span>
                     <div className={style.column_button_container}>
@@ -16,7 +16,7 @@ export const IntroTitle = ({ type, title, description, titleFontSize = 40, child
             )
         case 'row':
             return (
-                <section className={style.row}>
+                <section className={style.row} style={styles}>
                     <h1 className={style.row_title} style={{ fontSize: fontSize, lineHeight: fontSize }}>{title}</h1>
                     <div className={style.row_right}>
                         <span>Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.</span>
