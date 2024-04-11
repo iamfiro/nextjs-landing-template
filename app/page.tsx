4c627d41157bfad7b9bef9c380a353501d39060e
iamfiro <appturbo102@gmail.com>
2024-04-11 18:45:29 +0900

    🚀 Update IntroTitle component styles and import FaGithub icon in page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import { Button, Header, IntroTitle, Spacing, Sponsor } from "@/components";
 import Logo from "@/public/logo.png";
 import GoogleLogo from '@/public/sample-google.webp'
+import { FaGithub } from "react-icons/fa";
 
 export default function Home() {
     const headerPath = [
@@ -47,8 +48,8 @@ export default function Home() {
                 <Button href="/login" borderRadius="medium">무료로 시작하기</Button>
             </Header>
             <Spacing height={100} />
-            <IntroTitle type="column" title="무료 Next.js 랜딩 페이지 템플릿" description="세계 최대 기업에서 사용하는 Next.js를 사용하면 React 구성 요소의 강력한 기능으로 고품질 웹 애플리케이션을 만들 수 있습니다." titleFontSize={55}>
-                <Button href="/login" fontSize={18} borderRadius="big" styles={{ padding: '5px 30px'}}>시작하기</Button>
+            <IntroTitle type="column" title="The landing page template for NEXT.js developers" description="세계 최대 기업에서 사용하는 Next.js를 사용하면 React 구성 요소의 강력한 기능으로 고품질 웹 애플리케이션을 만들 수 있습니다." titleFontSize={55}>
+                <Button href="/login" fontSize={16} borderRadius="big" styles={{ padding: '5px 30px'}}><FaGithub /> Github</Button>
             </IntroTitle>
             <Spacing height={120} />
             <Sponsor title="Trust By" SponsorList={SponsorList} grayScaleMode={true} />
